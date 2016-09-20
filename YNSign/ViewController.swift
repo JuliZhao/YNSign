257c6df4c0e395cd727c4740c0a8c405e39df19a
Jwssica <Lynn@192.168.16.163>
2016-09-20 10:42:02 +0800

    签到

---
+++ YNSign/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  YNSign
+//
+//  Created by 李艳楠 on 16/9/20.
+//  Copyright © 2016年 Jessica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+
+    
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
